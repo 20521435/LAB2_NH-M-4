39aa692f015149bf3b16f0218758dacbc9af4791
20521435 <20521435@gm.uit.edu.vn>
2022-11-01 09:10:25 +0700

    LAB2_NHOM4_GYROSCOPE

---
+++ Debug/Drivers/BSP/Components/lis302dl/lis302dl.d
@@ -0,0 +1,6 @@
+Drivers/BSP/Components/lis302dl/lis302dl.o: \
+ ../Drivers/BSP/Components/lis302dl/lis302dl.c \
+ ../Drivers/BSP/Components/lis302dl/lis302dl.h \
+ d:\ce224\lab2\drivers\bsp\components\common\accelero.h
+../Drivers/BSP/Components/lis302dl/lis302dl.h:
+d:\ce224\lab2\drivers\bsp\components\common\accelero.h:
---
+++ Debug/Drivers/BSP/Components/lsm303agr/lsm303agr.d
@@ -0,0 +1,6 @@
+Drivers/BSP/Components/lsm303agr/lsm303agr.o: \
+ ../Drivers/BSP/Components/lsm303agr/lsm303agr.c \
+ ../Drivers/BSP/Components/lsm303agr/lsm303agr.h \
+ d:\ce224\lab2\drivers\bsp\components\common\accelero.h
+../Drivers/BSP/Components/lsm303agr/lsm303agr.h:
+d:\ce224\lab2\drivers\bsp\components\common\accelero.h:
